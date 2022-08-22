3021e4d96b62ea392c028743bda93f751c16ded4
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-08-22 14:11:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  function superbowlWin(recordList) {
+    for (const item of recordList) {
+        if (item.result === "W")
+        return item.year
+    }
+  }
